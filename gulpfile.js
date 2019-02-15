2baa47e5f6cedc392a92a2d63c3fc258920d7e5c
Erind Hoxha <erind_cbh@hotmail.com>
2019-02-15 13:17:34 +1300

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('compress', function (cb) {
 });
 
 gulp.task('cssMinify', function () {
-  return gulp.src('css/*.css')
+  return gulp.src('css/**/*.css')
     .pipe(cssMin())
     .pipe(gulp.dest('dist/css'));
 });
