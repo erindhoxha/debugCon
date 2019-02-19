0a0cb6bcf1593b4b9f1d4ae7d0dad9d34275e7a7
Denat Hoxha <denathoxha@gmail.com>
2019-02-19 23:01:28 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -264,7 +264,6 @@ hyjneshaNfron($('#hnn-pr'));
 $(function () {
 
   $('.md-trigger').on('click', function () {
-    console.log($(this).attr('data-nr'));
     var dataNr = $(this).attr('data-nr');
     $("#name-speaker").text(speakers.speakersks[dataNr].name);
     $("#occupation").text(speakers.speakersks[dataNr].occupation);
